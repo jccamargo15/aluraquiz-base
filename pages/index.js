4fb3fb7b6e0685045c0fc4c58daf1abc62333fff
José Camargo <josecamargo@Joses-MacBook-Pro.local>
2021-01-28 00:07:10 -0300

    código ao final da segunda aula da imersão react

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
+import React from 'react'
 import styled from 'styled-components'
-import db from '../db.json'
-import Widget from '../src/components/Widget'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+import { useRouter } from 'next/router'
+
+
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -21,9 +25,12 @@ export const QuizContainer = styled.div`
     margin: auto;
     padding: 15px;
   }
-`
+`;
 
 export default function Home() {
+  const router = useRouter()
+  const [name, setName] = React.useState('')
+
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
@@ -32,12 +39,32 @@ export default function Home() {
             <h1>Título Componente</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita amet voluptates doloremque veritatis quos exercitationem facilis hic provident ut non beatae inventore, vel reprehenderit soluta porro? Corrupti magnam asperiores voluptas.</p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault()
+              
+              router.push(`/quiz?name=${name}`)
+
+              console.log('Fazendo uma submissao por meio do react')
+
+              // router manda para 
+            }}>
+              <input
+                onChange={function (infosDoEvento) {
+                  console.log(infosDoEvento.target.value)
+                  // name = infosDoEvento.target.value
+                  setName(infosDoEvento.target.value)
+                }}
+                placeholder='Coloque seu nome'
+              />
+              <button type='submit' disabled={name.length === 0}>
+                Jogar {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
         <Widget>
-        <Widget.Header>
+          <Widget.Header>
             <h1>Título Componente</h1>
           </Widget.Header>
           <Widget.Content>
@@ -49,5 +76,5 @@ export default function Home() {
       </QuizContainer>
       <GitHubCorner projectUrl='google.com.br' />
     </QuizBackground>
-  )
+  );
 }
